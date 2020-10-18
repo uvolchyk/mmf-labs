d9752d8ad5ab3f10128db75873efb6c895076f05
Uladzislau Volchyk <uladzislau.volchyk@gmail.com>
2020-10-19 02:31:27 +0300

    feat: [ALGS] async gantt chart

--- algorithms/threads/code/chapt_2/task_1.py
+++ algorithms/threads/code/chapt_2/task_1.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 blocks = [[2,1,4,1],[1,3,2,3],[2,4,1,2],[3,2,3,1]]
 
-def ganttConverter(T):
+def gantt_converter_unjoint(T):
 
     result = []
     c_sum = 0
@@ -23,6 +23,38 @@ def ganttConverter(T):
             c_sum+=block
     return result
 
-c_df = pd.DataFrame(ganttConverter(blocks))
+def gantt_converter_async(T):
+
+    launch_moments = []
+    result = []
+
+    # constructing an array of launching moments
+    for i in range(len(T)):
+        start_anchor = 0
+        if i != 0:
+            for j in range(i):
+                start_anchor += T[i-1][j]
+        launch_moments.append(start_anchor)
+
+    for (i, process) in enumerate(T):
+        c_sum = launch_moments[i]
+        for (j, block_time) in enumerate(process):
+            result.append(
+                dict(
+                    process=i,
+                    block=j,
+                    duration=dt.datetime.fromtimestamp(block_time+c_sum).strftime('%Y-%m-%d %H:%M:%S'),
+                    start=dt.datetime.fromtimestamp(c_sum).strftime('%Y-%m-%d %H:%M:%S')
+                )
+            )
+            c_sum += block_time
+    return result
+
+    
+
+
+# gantt_converter_async(blocks)
+
+c_df = pd.DataFrame(gantt_converter_async(blocks))
 c_fig = px.timeline(c_df, x_start="start", x_end="duration", y="process", color="block")
 c_fig.show()
