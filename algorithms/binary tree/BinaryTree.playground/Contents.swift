5d9223ec0d1340838c6c77b7466d56007e7ac473
Uladzislau Volchyk <uladzislau.volchyk@gmail.com>
2020-11-16 01:55:36 +0300

    fix: [ALGS] improved trees drawing

--- algorithms/binary tree/BinaryTree.playground/Contents.swift
+++ algorithms/binary tree/BinaryTree.playground/Contents.swift
@@ -1,4 +1,4 @@
-
+import Foundation
 // MARK: -
 
 class Node<Value: Comparable> {
@@ -51,6 +51,10 @@ extension Node {
     
     func postorder(node: Node?, padding: Int = 0) {
         if let node = node {
+            postorder(node: node.right, padding: padding + 4)
+            if padding != 0 {
+                print("")
+            }
             postorder(node: node.left, padding: padding + 1)
             postorder(node: node.right, padding: padding + 1)
             print(String(repeating: " ", count: padding) + " \(node.value) \n")
@@ -98,11 +102,96 @@ extension Node {
 }
 
 var testMeta = (1...16).map { _ -> Int in Int.random(in: 0...150) }.sorted()
-print(testMeta)
+print("Сгенерированный массив: \(testMeta)")
+print("Корневой элемент: \(testMeta[8])")
 let root = Node<Int>(value: testMeta[8])
 testMeta.remove(at: 8)
 testMeta.forEach { root.insert(value: $0) }
 
-root.postorder(node: root)
 
-print("\(root.searchBelow(for: 20)?.value ?? 0) <-")
+func treeString<T>(_ node:T, reversed:Bool=false, isTop:Bool=true, using nodeInfo:(T)->(String,T?,T?)) -> String
+{
+   let (stringValue, leftNode, rightNode) = nodeInfo(node)
+
+   let stringValueWidth  = stringValue.count
+
+   let leftTextBlock     = leftNode  == nil ? []
+                         : treeString(leftNode!,reversed:reversed,isTop:false,using:nodeInfo)
+                           .components(separatedBy:"\n")
+
+   let rightTextBlock    = rightNode == nil ? []
+                         : treeString(rightNode!,reversed:reversed,isTop:false,using:nodeInfo)
+                           .components(separatedBy:"\n")
+
+   let commonLines       = min(leftTextBlock.count,rightTextBlock.count)
+   let subLevelLines     = max(rightTextBlock.count,leftTextBlock.count)
+
+   let leftSubLines      = leftTextBlock
+                         + Array(repeating:"", count: subLevelLines-leftTextBlock.count)
+   let rightSubLines     = rightTextBlock
+                         + Array(repeating:"", count: subLevelLines-rightTextBlock.count)
+
+   let leftLineWidths    = leftSubLines.map{$0.count}
+   let rightLineIndents  = rightSubLines.map{$0.prefix{$0==" "}.count  }
+
+   let firstLeftWidth    = leftLineWidths.first   ?? 0
+   let firstRightIndent  = rightLineIndents.first ?? 0
+
+   let linkSpacing       = min(stringValueWidth, 2 - stringValueWidth % 2)
+   let leftLinkBar       = leftNode  == nil ? 0 : 1
+   let rightLinkBar      = rightNode == nil ? 0 : 1
+   let minLinkWidth      = leftLinkBar + linkSpacing + rightLinkBar
+   let valueOffset       = (stringValueWidth - linkSpacing) / 2
+
+   let minSpacing        = 2
+   let rightNodePosition = zip(leftLineWidths,rightLineIndents[0..<commonLines])
+                           .reduce(firstLeftWidth + minLinkWidth)
+                           { max($0, $1.0 + minSpacing + firstRightIndent - $1.1) }
+
+   let linkExtraWidth    = max(0, rightNodePosition - firstLeftWidth - minLinkWidth )
+   let rightLinkExtra    = linkExtraWidth / 2
+   let leftLinkExtra     = linkExtraWidth - rightLinkExtra
+
+   let valueIndent       = max(0, firstLeftWidth + leftLinkExtra + leftLinkBar - valueOffset)
+   let valueLine         = String(repeating:" ", count:max(0,valueIndent))
+                         + stringValue
+   let slash             = reversed ? "\\" : "/"
+   let backSlash         = reversed ? "/"  : "\\"
+   let uLine             = reversed ? "¯"  : "_"
+   
+   let leftLink          = leftNode == nil ? ""
+                         : String(repeating: " ", count:firstLeftWidth)
+                         + String(repeating: uLine, count:leftLinkExtra)
+                         + slash
+    
+   let rightLinkOffset   = linkSpacing + valueOffset * (1 - leftLinkBar)
+   let rightLink         = rightNode == nil ? ""
+                         : String(repeating:  " ", count:rightLinkOffset)
+                         + backSlash
+                         + String(repeating:  uLine, count:rightLinkExtra)
+
+   let linkLine          = leftLink + rightLink
+
+   let leftIndentWidth   = max(0,firstRightIndent - rightNodePosition)
+   let leftIndent        = String(repeating:" ", count:leftIndentWidth)
+   let indentedLeftLines = leftSubLines.map{ $0.isEmpty ? $0 : (leftIndent + $0) }
+
+   let mergeOffsets      = indentedLeftLines
+                           .map{$0.count}
+                           .map{leftIndentWidth + rightNodePosition - firstRightIndent - $0 }
+                           .enumerated()
+                           .map{ rightSubLines[$0].isEmpty ? 0  : $1 }
+
+   let mergedSubLines    = zip(mergeOffsets.enumerated(),indentedLeftLines)
+                           .map{ ( $0.0, $0.1, $1 + String(repeating:" ", count:max(0,$0.1)) ) }
+                           .map{ $2 + String(rightSubLines[$0].dropFirst(max(0,-$1))) }
+
+   let treeLines = [leftIndent + valueLine]
+                 + (linkLine.isEmpty ? [] : [leftIndent + linkLine])
+                 + mergedSubLines
+
+   return (reversed && isTop ? treeLines.reversed(): treeLines)
+          .joined(separator:"\n")
+}
+
+print(treeString(root) { (node) -> (String, Node<Int>?, Node<Int>?) in return ("\(node.description)", node.left, node.right)})
--- algorithms/btree/swift/BTree.playground/Contents.swift
+++ algorithms/btree/swift/BTree.playground/Contents.swift
@@ -1,14 +1,14 @@
 // MARK: Node
 
-class BTreeNode<Key: Comparable, Value> {
+class BTreeNode<Key: Comparable, Value: CustomStringConvertible> {
     
     let owner: BTree<Key, Value>
     
     var keys: Array<Key> = []
     var values: Array<Value> = []
-    var children: Array<BTreeNode>?
+    var children: Array<BTreeNode> = []
     
-    var isLeaf: Bool { children == nil }
+    var isLeaf: Bool { children.isEmpty }
     var numberOfKeys: Int { keys.count }
     
     init(owner: BTree<Key, Value>) {
@@ -22,7 +22,7 @@ class BTreeNode<Key: Comparable, Value> {
         self.init(owner: owner)
         self.keys += keys
         self.values += values
-        self.children = children
+        self.children = children ?? []
     }
 }
 
@@ -51,9 +51,9 @@ extension BTreeNode {
             values.insert(value, at: index)
             owner.numberOfKeys = (owner.numberOfKeys + 1)
         } else {
-            children![index].insert(value, for: key)
-            if children![index].numberOfKeys > owner.order {
-                split(children![index], at: index)
+            children[index].insert(value, for: key)
+            if children[index].numberOfKeys > owner.order {
+                split(children[index], at: index)
             }
         }
     }
@@ -73,46 +73,47 @@ extension BTreeNode {
         child.keys.removeSubrange(child.keys.indices.suffix(from: middle))
         child.values.removeSubrange(child.values.indices.suffix(from: middle))
         
-        children!.insert(newSibling, at: index + 1)
+        children.insert(newSibling, at: index + 1)
         
-        if child.children != nil {
+        if !child.children.isEmpty {
             newSibling.children = Array(
-                child.children![child.children!.indices.suffix(from: middle + 1)]
+                child.children[child.children.indices.suffix(from: middle + 1)]
             )
-            child.children!.removeSubrange(child.children!.indices.suffix(from: middle + 1))
+            child.children.removeSubrange(child.children.indices.suffix(from: middle + 1))
         }
     }
 }
 
 extension BTreeNode: CustomStringConvertible {
-    var description: String {
-        var str = "\(keys)"
-        if !isLeaf { children?.forEach { str.append("\n\($0.description)\n") } }
-        return str
-    }
+    var description: String { keys.description }
+    
+    func treeLines(_ nodeIndent:String = "", _ childIndent:String="") -> [String] {
+        var result = ["\(nodeIndent)\(description)"]
+        result.append(contentsOf: children.enumerated().map { ($0 < children.count - 1, $1) }
+                                     .flatMap{ $0 ? $1.treeLines("┣╸","┃ ") : $1.treeLines("┗╸","  ") }
+                                     .map{ childIndent + $0 })
+        
+        return result
+       }
+
+    func printTree() { print(treeLines().joined(separator:"\n")) }
 }
 
 extension BTreeNode {
-    func traverseKeysPostOrder(_ process: (Key) -> Void) {
-        children?.last?.traverseKeysPostOrder(process)
-        for i in (0..<numberOfKeys).reversed() {
-            children?[i].traverseKeysPostOrder(process)
-            process(keys[i])
+    func traverseKeysPostOrder(_ process: (BTreeNode) -> Void) {
+        children.last?.traverseKeysPostOrder(process)
+        children.reversed().enumerated().forEach { (i, child) in
+            if !child.children.isEmpty {
+                child.traverseKeysPostOrder(process)
+            }
+            process(child)
         }
-    }
-    func traverseKeysInOrder(_ process: (Key) -> Void) {
-      for i in 0..<numberOfKeys {
-        children?[i].traverseKeysInOrder(process)
-        process(keys[i])
-      }
-
-      children?.last?.traverseKeysInOrder(process)
     }
 }
 
 // MARK: Tree
 
-class BTree<Key: Comparable, Value> {
+class BTree<Key: Comparable, Value: CustomStringConvertible> {
     let order: Int
     
     lazy var rootNode: BTreeNode<Key, Value> = BTreeNode<Key, Value>(owner: self)
@@ -149,32 +150,36 @@ class BTree<Key: Comparable, Value> {
         rootNode.keys.removeSubrange(rootNode.keys.indices.suffix(from: middle))
         rootNode.values.removeSubrange(rootNode.values.indices.suffix(from: middle))
 
-        if rootNode.children != nil {
+        if !rootNode.children.isEmpty {
             newSibling.children = Array(
-                rootNode.children![rootNode.children!.indices.suffix(from: middle + 1)]
+                rootNode.children[rootNode.children.indices.suffix(from: middle + 1)]
             )
-            rootNode.children!.removeSubrange(rootNode.children!.indices.suffix(from: middle + 1))
+            rootNode.children.removeSubrange(rootNode.children.indices.suffix(from: middle + 1))
         }
 
-        newRoot.children!.append(newSibling)
+        newRoot.children.append(newSibling)
         rootNode = newRoot
     }
     
 }
 
 
-
 let tree = BTree<Int, Int>(order: 5)
 
 let initialMeta = (1...16).map { _ -> Int in Int.random(in: 0...150) }.sorted()
 let testMeta = Array(initialMeta.suffix(from: 8)) + Array(initialMeta.prefix(upTo: 8))
+print("Сгенерированные данные")
 print(testMeta.enumerated().map { "\($0) - \($1)"})
 testMeta.enumerated().forEach { (n, x) in
     tree.insert(x, for: n)
 }
 
-tree.rootNode.traverseKeysPostOrder { (key) in
-    print(key)
-}
+print("Представление дерева")
+tree.rootNode.printTree()
 
-print("\(tree.rootNode.value(for: 4)) <- value")
+print("Результат поиска снизу\n \(tree.rootNode.value(for: 4)) <- value")
+
+print("Результат обратного обхода")
+tree.rootNode.traverseKeysPostOrder({ (node) in
+    print(node.keys)
+})
